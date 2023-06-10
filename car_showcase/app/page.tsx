9d0916a2ced5392c294e3af78c660971053a4e9e
ubAI6689 <ubAI6689@gmail.com>
2023-06-10 19:20:35 +0800

    Car details not working

--- car_showcase/app/page.tsx
+++ car_showcase/app/page.tsx
@@ -1,6 +1,13 @@
-import { CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars();
+  console.log(allCars);
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -15,12 +22,31 @@ export default function Home() {
         <div className='home__filters'>
           <SearchBar/>
 
-          <div className='home__filters-container'>
+          <div className='home__filter-container'>
             <CustomFilter title='fuel'/>
             <CustomFilter title='year'/> 
           </div>
         </div>
         
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+
+            </div>
+          </section>
+        ): (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>
+                No results
+            </h2>
+            <p>{allCars?.message}</p>
+
+          </div>
+        )}
+
       </div>
     </main>
   )
