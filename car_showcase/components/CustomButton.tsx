cc7404a8828b85230a57c82d8f4fc09026d36e87
ubAI6689 <ubAI6689@gmail.com>
2023-06-10 17:21:04 +0800

    Combobox

--- car_showcase/components/CustomButton.tsx
+++ car_showcase/components/CustomButton.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-const CustomButton = () => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
     <button 
       disabled={false}
-      type={"button"}
-      className={`custom-btn`}
+      type={ btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
       onClick={() => {}}
     >
+
+      <span className={`flex-1`}>
+        {title}
+      </span>
       
     </button>
   )
