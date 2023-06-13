ebe58fa6d0e67ce3acd1ced33197a3e19bf65495
ubAI6689 <ub.AI6689@gmail.com>
2023-06-13 16:33:33 +0800

    edit implemented

--- memories_project/server/controllers/posts.js
+++ memories_project/server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
     try {
@@ -21,4 +22,15 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params; // Renaming id to _id
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id");
+    
+    const updatedPost =  await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost);
 }
