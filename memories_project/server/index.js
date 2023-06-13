b3577796a5a82ffa2f0d5a1ce563242d85bb500e
ubAI6689 <ub.AI6689@gmail.com>
2023-06-13 15:19:00 +0800

    Done 1st part

--- memories_project/server/index.js
+++ memories_project/server/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes); // http://localhost:5000/posts
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use('/posts', postRoutes); // http://localhost:5000/posts
 
 const CONNECTION_URL = 'mongodb+srv://memories:memories123@memories.e0vycym.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
