5c7c2e20a2c3aac7d05016f1f298d23d2aa73875
ubAI6689 <ub.ai6689@gmail.com>
2023-06-13 18:40:38 +0700

    update

--- car_showcase/components/CarDetails.tsx
+++ car_showcase/components/CarDetails.tsx
@@ -4,7 +4,7 @@ import { Fragment } from "react";
 import Image from "next/image";
 
 import { Dialog, Transition } from "@headlessui/react";
-import { CarProps } from "@types";
+import { CarProps } from "@/types";
 
 interface CarDetailsProps {
   isOpen: boolean;
