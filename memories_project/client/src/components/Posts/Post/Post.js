03eac7e65d03fe2f83b63948d60097d3f15b0a41
ubAI6689 <ub.AI6689@gmail.com>
2023-06-13 16:41:55 +0800

    Implemented DELETE

--- memories_project/client/src/components/Posts/Post/Post.js
+++ memories_project/client/src/components/Posts/Post/Post.js
@@ -4,11 +4,16 @@ import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import moment from "moment";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-
 import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+
+import { deletePost } from "../../../actions/posts";
+
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -32,7 +37,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
